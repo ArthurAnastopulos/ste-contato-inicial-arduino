6b21a6a457e6c8528d612984503fb7696d085278
Arthur Anastopulos <arthur.santos@khomp.com>
2022-09-13 21:02:36 -0300

    feat(programa1): Solução Programa 1

--- README.md
+++ README.md
@@ -12,8 +12,28 @@ Utilize o kit com Arduino entregue a você para implementar dois programas com a
 3. O código-fonte e a documentação do seu trabalho deve estar no GitHub. A documentação deve estar no arquivo README.md do diretório da aplicação, e deve incluir o esquema elétrico utilizado e o(s) diagrama(s) de máquina(s) de estado.
 
 Dica 1: esta solução foi feita em aula. Reaproveite as informações/código desenvolvidos com o professor e organize sua entrega.
+### Documentação: Programa 1 - Interruptor LED
 
+Para o Primeiro Programa foi desenvolvido um sistema de Interruptor LED, foi utilizada a solução feita em aula como base. Onde o circuito do sistema segue na imagem a seguir:
 
+![Circuito01](/images/circuito1.png)
+
+Para este circuito foram utilizados os seguintes componentes:
+* Entrada: Interruptor (botão);
+* Saída: LED;
+* Um resistor de 220 Ohms para o LED;
+* Um resistor de 10 kOhms para o Interruptor.
+
+Já sua Máquina de Estado,também utilizando como base a solução feita em aula. Possui o seguinte modelo:
+
+![State01](/images/StateMachine1.png)
+
+Como visto na imagem esta possui 4 Estados:
+
+* **OFF**: Que representa o estado do LED desligado;
+* **E1**: Que representa o estado intermediário de OFF para ON, neste momento já acionado o LED;
+* **ON**: Que representa o estado do LED ligado;
+* **E2**: Que representa o estado intermediário de ON para OFF, neste momento já acionado o LED
 ## Programa 2 - Voltímetro Digital
 
 Documente a solução e implemente um voltímetro digital, conforme debatido em sala de aula. O sistema embarcado desenvolvido deve ter as seguintes propriedades:
---
+++ programa01.ino
@@ -0,0 +1,100 @@
+volatile int botao = 0;
+int led = 0;
+
+enum State_t
+{
+  OFF = 0,
+  E1 = 1,
+  ON = 2,
+  E2 = 3
+};
+State_t state = OFF;
+
+void botao_isr()
+{
+  static unsigned long ultima_interrupcao = 0;
+  static unsigned long tempo_botao = 50;
+
+  unsigned long agora = millis();
+
+  if ((agora - ultima_interrupcao) >= tempo_botao )
+  {
+    Serial.println("b1");
+    botao = 1;
+    ultima_interrupcao = agora;
+  }
+  //Serial.println("b2");
+}
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+
+  state = OFF;
+  Serial.println("OFF");
+
+  attachInterrupt(digitalPinToInterrupt(2), botao_isr, RISING);
+}
+
+void loop()
+{
+  // Atualiza entradas
+  noInterrupts();
+  int b = botao;
+  botao = 0;
+  interrupts();
+
+  // Atualiza máquina de estados
+  switch (state)
+  {
+    case OFF:
+      if (b == 1)
+      {
+        state = E1;
+        Serial.println("E1");
+        attachInterrupt(digitalPinToInterrupt(2), botao_isr, LOW);
+        led = 1;
+      }
+      break;
+
+    case E1:
+      if (b == 1)
+      {
+        state = ON;
+        Serial.println("ON");
+        attachInterrupt(digitalPinToInterrupt(2), botao_isr, RISING);
+        led = 1;
+      }
+      break;
+
+    case ON:
+      if (b == 1)
+      {
+        state = E2;
+        Serial.println("E2");
+        attachInterrupt(digitalPinToInterrupt(2), botao_isr, LOW);
+        led = 0;
+      }
+      break;
+
+    case E2:
+      if (b == 1)
+      {
+        state = OFF;
+        Serial.println("OFF");
+        attachInterrupt(digitalPinToInterrupt(2), botao_isr, RISING);
+        led = 0;
+      }
+      break;
+
+    default: /* não deveria acontecer */
+      break;
+  }
+
+  // Atualiza saídas
+  digitalWrite(13, led);
+
+  delay(50);
+}
